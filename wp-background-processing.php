b510832991c2e39f3f68fa8c6ada428077d8dccb
Joseph D'Souza <joseph.dsouza@alphalabs.net>
2021-12-09 12:35:23 +0000

    Make an independant version of class names to avoid clashes without having to use something like 'Mozart'

--- wp-background-processing.php
+++ wp-background-processing.php
@@ -16,9 +16,5 @@ GitHub Plugin URI: https://github.com/A5hleyRich/wp-background-processing
 GitHub Branch: master
 */
 
-if ( ! class_exists( 'WP_Async_Request' ) ) {
-	require_once plugin_dir_path( __FILE__ ) . 'classes/wp-async-request.php';
-}
-if ( ! class_exists( 'WP_Background_Process' ) ) {
-	require_once plugin_dir_path( __FILE__ ) . 'classes/wp-background-process.php';
-}
+require_once plugin_dir_path( __FILE__ ) . 'classes/wp-async-request.php';
+require_once plugin_dir_path( __FILE__ ) . 'classes/wp-background-process.php';
